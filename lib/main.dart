b8fbdc188dab2737ee88f378fb6042d0d0f27d69
Gautam Rajbhar <Gautam.Rajbhar_Ext@ltts.com>
2022-02-07 15:38:29 +0530

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'dialog_utils.dart';
-
 void main() {
-  runApp(MaterialApp(home: MyWidget()) );
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyWidget()));
 }
-
 
 class MyWidget extends StatefulWidget {
   const MyWidget({Key? key}) : super(key: key);
@@ -15,68 +12,43 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-
-  String txt = "Alert Input Box";
+  String txt = "DIALOG";
 
   TextEditingController alertController = TextEditingController();
 
-
-
-  myAlertDialog(BuildContext context){
-
+  myAlertDialog(BuildContext context) {
     return showDialog<String>(
         context: context,
         builder: (BuildContext context) => AlertDialog(
-          title: Text("Enter Name"),
-          content: TextField(controller: alertController,),
-          actions: <Widget>[
+              title: Text("Enter Name"),
+              content: TextField(
+                controller: alertController,
+              ),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context, 'cancel');
+                    },
+                    child: Text("Cancel")),
+                TextButton(
+                    onPressed: () {
+                      print(alertController.text);
 
-            TextButton(
-                onPressed: (){
-                  Navigator.pop(context,'cancel');
-                },
-                child: Text("Cancel")
-            ),
-            TextButton(
-                onPressed: (){
-
-
-
-                  print(alertController.text);
-
-                  setState(() {
-                    txt = alertController.text;
-                  });
-                  Navigator.pop(context,'ok');
-                  alertController.text = "";
-
-                },
-                child: Text("Ok")
-            ),
-
-          ],
-
-
-        ));
-
-
-
-
+                      setState(() {
+                        txt = alertController.text;
+                      });
+                      Navigator.pop(context, 'ok');
+                      alertController.text = "";
+                    },
+                    child: Text("Ok")),
+              ],
+            ));
   }
-
-
-
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text("$txt")),
         body: Center(
@@ -84,102 +56,143 @@ class _MyWidgetState extends State<MyWidget> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                  onPressed: (){
-
+                  onPressed: () {
                     Dialog errorDialog = Dialog(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)), //this right here
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(12.0)), //this right here
                       child: Container(
                         height: 300.0,
                         width: 300.0,
-
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Padding(
-                              padding:  EdgeInsets.all(15.0),
-                              child: Text('Cool', style: TextStyle(color: Colors.red),),
+                              padding: EdgeInsets.all(15.0),
+
+                              child: Container(
+                                height: 100.0,
+                                width: 100.0,
+                                child: Image.asset("assets/images/alert1.jfif"),
+                              ),
+                              //child: Text('Cool', style: TextStyle(color: Colors.red,fontSize: 20.0),),
                             ),
                             Padding(
                               padding: EdgeInsets.all(15.0),
-                              child: Text('Awesome', style: TextStyle(color: Colors.red),),
+                              child: Text(
+                                'Awesome',
+                                style: TextStyle(
+                                    color: Colors.teal,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0),
+                              ),
                             ),
                             Padding(padding: EdgeInsets.only(top: 50.0)),
-                            TextButton(onPressed: () {
+                            TextButton(
+                                onPressed: () {
+                                  print("Do Something.....");
 
-                              print("Do Something.....");
-
-
-                              Navigator.of(context).pop();
-                            },
-                                child: Text('Got It!', style: TextStyle(color: Colors.purple, fontSize: 18.0),))
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  'Got It!',
+                                  style: TextStyle(
+                                      color: Colors.purple,
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold),
+                                ))
                           ],
                         ),
                       ),
                     );
 
-                    showDialog(context: context, builder: (BuildContext context) => errorDialog);
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) => errorDialog);
                   },
-                  child: Text("First Dialog")),
-
-
-
-
+                  child: Text("Dialog")),
               ElevatedButton(
-                  onPressed: (){
-
-
+                  onPressed: () {
                     myAlertDialog(context);
-
                   },
-                  child: Text("Second Dialog")),
-
-
-
-
+                  child: Text("Input Dialog")),
               ElevatedButton(
-                  onPressed: (){
-
-
+                  onPressed: () {
                     showDialog<String>(
                       context: context,
                       builder: (BuildContext context) => AlertDialog(
-                        title: const Text('AlertDialog Title'),
-                        content: const Text('AlertDialog description'),
+                        title: const Text('Abort'),
+                        content: const Text(
+                            'Are you sure you want to abort this operation?'),
                         actions: <Widget>[
                           TextButton(
                             onPressed: () {
-
                               print("Cancel Cancel Cancel Cancel");
 
-                    Navigator.pop(context, 'Cancel');
-
-                    } ,
+                              Navigator.pop(context, 'Cancel');
+                            },
                             child: const Text('Cancel'),
                           ),
                           TextButton(
                             onPressed: () {
-
-
                               print("Ok Ok Ok Ok");
 
                               Navigator.pop(context, 'OK');
-
-                    },
-                            child: const Text('OK'),
+                            },
+                            child: const Text('Continue'),
                           ),
                         ],
                       ),
                     );
-
-
-
-
-
-
                   },
-                  child: Text("Third Dialog")),
+                  child: Text("Alert Dialog")),
               ElevatedButton(
-                  onPressed: (){},
+                  onPressed: () {
+                    Dialog errorDialog = Dialog(
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(20.0)), //this right here
+                      child: Container(
+                        color: Colors.deepPurple,
+                        height: 300.0,
+                        width: 300.0,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.all(0.0),
+
+                              child: Container(
+                                height: 250.0,
+                                width: 250.0,
+                                child: Image.asset(
+                                    "assets/images/download11.jfif"),
+                              ),
+                              //child: Text('Cool', style: TextStyle(color: Colors.red,fontSize: 20.0),),
+                            ),
+                            Padding(padding: EdgeInsets.only(top: 0.0)),
+                            TextButton(
+                                onPressed: () {
+                                  print("Do Something.....");
+
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  'Ok',
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.bold),
+                                ))
+                          ],
+                        ),
+                      ),
+                    );
+
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) => errorDialog);
+                  },
                   child: Text("Fourth Dialog")),
             ],
           ),
